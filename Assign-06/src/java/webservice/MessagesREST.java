cde7c498ce3fec70706c74ae516d658af04e4054
Vaishali Somu <vaishalisomu12feb@gmail.com>
2017-03-22 15:55:17 -0400

    Implemented Webservices

--- Assign-06/src/java/webservice/MessagesREST.java
+++ Assign-06/src/java/webservice/MessagesREST.java
@@ -5,6 +5,15 @@
  */
 package webservice;
 
+import controller.Messages;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
@@ -12,6 +21,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -20,34 +30,53 @@ import javax.ws.rs.core.MediaType;
  * @author c0687174
  */
 @Path("messages")
+@ApplicationScoped
 public class MessagesREST {
 
-    @Context
-    private UriInfo context;
+    @Inject
+    private Messages messages;
 
     /**
-     * Creates a new instance of MessagesREST
-     */
-    public MessagesREST() {
-    }
-
-    /**
-     * Retrieves representation of an instance of webservice.MessagesREST
-     * @return an instance of java.lang.String
+     * Retrieves all the messages in Json
+     *
+     * @return Array of Json objects
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getJson() {
-        //TODO return proper representation object
-        throw new UnsupportedOperationException();
+    public JsonArray getJson() {
+        return messages.getJsonMessages();
     }
 
     /**
-     * PUT method for updating or creating an instance of MessagesREST
-     * @param content representation for the resource
+     * Retrieves the json with the particular id
+     *
+     * @param id Id of the message
+     * @return json of the message
      */
-    @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
-    public void putJson(String content) {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{id}")
+    public JsonObject getJsonById(@PathParam("id") int id) {
+        return messages.getMessageById(id).getJson();
+    }
+
+    /**
+     * Retrieves all the messages between the dates
+     *
+     * @param fromDate from date of the mesages
+     * @param toDate to date of he messages
+     * @return Array of Json messages
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{fromDate}/{toDate}")
+    public JsonArray getJsonByDate(@PathParam("fromDate") String fromDate, @PathParam("toDate") String toDate) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+        try {
+            return messages.getMessagesByDate(sdf.parse(fromDate), sdf.parse(toDate));
+        } catch (ParseException ex) {
+            Logger.getLogger(MessagesREST.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
     }
 }
