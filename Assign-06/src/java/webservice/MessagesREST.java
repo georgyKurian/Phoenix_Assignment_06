43117f9ad0e07811c7113f767befae24ba5b421b
georgy varghese kurian <c0687988@mylambton.ca>
2017-03-22 22:08:41 -0400

    Implemented web service

--- Assign-06/src/java/webservice/MessagesREST.java
+++ Assign-06/src/java/webservice/MessagesREST.java
@@ -6,23 +6,28 @@
 package webservice;
 
 import controller.Messages;
+import java.io.StringReader;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * REST Web Service
@@ -78,5 +83,55 @@ public class MessagesREST {
             Logger.getLogger(MessagesREST.class.getName()).log(Level.SEVERE, null, ex);
             return null;
         }
+    }
+    
+    /**
+     * Inserts the message send
+     *
+     * @param content Json from the client
+     * @return Inserted Json with id
+     */
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject postJson(String content) {
+        JsonObject json = Json.createReader(new StringReader(content)).readObject();
+        return messages.insertMessage(json);
+    }
+
+    /**
+     * Updates a particular message details
+     *
+     * @param content Json with uodated details
+     * @param id Id of the message
+     * @return Updated Json of the message
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{id}")
+    public JsonObject putJson(String content, @PathParam("id") int id) {
+
+        JsonObject json = Json.createReader(new StringReader(content)).readObject();
+
+        return messages.updateMessageById(id, json).getJson();
+
+    }
+
+    /**
+     * Deletes the message with the Id
+     *
+     * @param id The id of the message
+     * @return response code
+     */
+    @DELETE
+    @Path("{id}")
+    public Response deleteMessage(@PathParam("id") int id) {
+        if (messages.deleteMessageById(id)) {
+            return Response.status(200).entity(id).build();
+        } else {
+            return Response.status(401).build();
+        }
+
     }
 }
